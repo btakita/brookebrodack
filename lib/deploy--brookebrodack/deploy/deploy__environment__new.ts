4ea5fcacd54f0f1a5352a37f51843b0ddb89da47
Brian Takita <brian.takita@gmail.com>
2024-01-28 21:22:08 -0500

    deploy--brookebrodack: deploy__environment__new: ctx: Ctx_wide_T→∋root_ctx_T
    submodule:
    
    	domain--any--blog: ./theme:
    		theme: ∋ .add
    		∋ root_ctx_T
    	domain--browser--brookebrodack: ./ctx: browser_ctx: ∋ blog__ctx__new→blog_ctx__new
    	domain--server--blog: ctx: blog_server__ctx__new: ∋ blog__ctx__new→blog_ctx__new
    	ui--server--brookebrodack: layout_c_: - ∋ script_: window.va

--- lib/deploy--brookebrodack/deploy/deploy__environment__new.ts
+++ lib/deploy--brookebrodack/deploy/deploy__environment__new.ts
@@ -1,7 +1,7 @@
-import { type Ctx_wide_T } from 'ctx-core/be'
+import { type root_ctx_T } from '@btakita/domain--any--blog/src/ctx/root_ctx.ts'
 import { deploy__env } from './deploy__env.js'
 import { NODE_ENV_ } from './stage.js'
-export function deploy__environment__new(ctx:Ctx_wide_T<''>) {
+export function deploy__environment__new(ctx:root_ctx_T) {
 	const NODE_ENV = NODE_ENV_(ctx)
 	return {
 		BROOKEBRODACK_PORT: deploy__env.BROOKEBRODACK_PORT,
