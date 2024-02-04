e3799b59dcd0934575e18ac4b609ebb8c106bf32
Brian Takita <brian.takita@gmail.com>
2024-02-03 19:06:04 -0500

    submodule:
    	brookebrodack-site:
    		/brookers:
    			browser: hy__bind: brookers__page__hy__bind__id→brookers
    		tsconfig.json: - allowImportingTsExtensions
    		update dependencies
    	domain--any--blog: imports: *.js
    	domain--any--brookebrodack: tsconfig.json: - allowImportingTsExtensions
    	domain--browser--brookebrodack: tsconfig.json: - allowImportingTsExtensions
    	domain--server--blog:
    		imports: *.js
    		update dependencies
    	ui--any--blog:
    		imports: *.js
    		update dependencies
    	ui--any--brookebrodack:
    		- ./brookers: - brookers__page__hy__bind__id
    		./css: bg_c_→bg__tag_
    	ui--browser--brookebrodack:
    		./brookers:
    			brookers__page__bind→brookers__doc_html__bind
    		tsconfig.json: - allowImportingTsExtensions
    	ui--server--blog: update dependencies
    	ui--server--brookebrodack:
    		./brookers: brookers__page_→brookers__doc_html_
    			hy__bind: brookers__page__hy__bind__id→brookers
    			.brookers__page__hero_c→.brookers_hero__div
    			back_link_c_→brookers_back_link__a_
    			brookers__page__detail_c_→brookers_detail__div_
    			#html_op__top→#html_op_top__div
    			#op__title→#op_title__div
    			#html_op__close→#html_op_close__div
    			brookers__page__master_c_→brookers_master__div_:
    				+ .hidden: fix: fouc
    			brookers__timeline_c_→brookers_timeline__ol_
    			brookers__timeline__item_c_→brookers_timeline__li_
    			brookers__timeline_c_→brookers_timeline__ol_
    		cite_→cite__sup_
    			./home: home__page_→home__doc_html_
    			./layout: layout_c_→layout__doc_html_:
    				+ html_props?:tag_props_T&{ class?:string }
    			imports: *.js
    			tsconfig.json: - allowImportingTsExtensions
    			update dependencies
    
    update dependencies

--- lib/deploy--brookebrodack/deploy/deploy__environment__new.ts
+++ lib/deploy--brookebrodack/deploy/deploy__environment__new.ts
@@ -1,4 +1,4 @@
-import { type root_ctx_T } from '@btakita/domain--any--blog/src/ctx/root_ctx.ts'
+import { type root_ctx_T } from '@btakita/domain--any--blog'
 import { deploy__env } from './deploy__env.js'
 import { NODE_ENV_ } from './stage.js'
 export function deploy__environment__new(ctx:root_ctx_T) {
