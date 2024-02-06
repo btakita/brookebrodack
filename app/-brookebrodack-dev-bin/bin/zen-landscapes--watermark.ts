6ae31abcc11dc407947981ab421778666af7cba5
Brian Takita <brian.takita@gmail.com>
2024-02-06 01:12:12 -0500

    -brookebrodack-dev-bin:
    	zen-landscapes--watermark: clamp watermarked images
    	+ product--download--sync
    	+ product--download
    	+ product--upload--sync
    	+ product--upload

--- app/-brookebrodack-dev-bin/bin/zen-landscapes--watermark.ts
+++ app/-brookebrodack-dev-bin/bin/zen-landscapes--watermark.ts
@@ -8,6 +8,8 @@ const dir = dirname(new URL(import.meta.url).pathname)
 const in_dir = resolve(`${dir}/../public/product/zen-landscapes`)
 const out_dir = resolve(`${dir}/../public/assets/images/zen-landscapes`)
 const wm_path = resolve(`${dir}/../public/assets/images/brooke-brodack-logo.jpg`)
+const clamp_width = 1024
+const clamp_height = 1024
 const queue = queue_(4)
 let count = 0
 let total = 0
@@ -16,7 +18,9 @@ for (const in_basename of await readdir(in_dir)) {
 	queue.add(async ()=>{
 		const in_extname = extname(in_basename)
 		const in_image = await loadImage(join(in_dir, in_basename))
-		const canvas = createCanvas(in_image.width, in_image.height)
+		const clamp_o =
+			clamp_o_(in_image.width, in_image.height, clamp_width, clamp_height)
+		const canvas = createCanvas(clamp_o.width, clamp_o.height)
 		const ctx = canvas.getContext('2d')
 		ctx.drawImage(in_image, 0, 0, canvas.width, canvas.height)
 		const wm_image = await loadImage(wm_path)
@@ -32,3 +36,15 @@ for (const in_basename of await readdir(in_dir)) {
 spinner.text = count + ' of ' + total
 await queue.close()
 spinner.stop()
+function clamp_o_(width:number, height:number, clamp_width:number, clamp_height:number) {
+	const aspectRatio = width / height
+	if (width > clamp_width) {
+		width = clamp_width
+		height = width / aspectRatio
+	}
+	if (height > clamp_height) {
+		height = clamp_height
+		width = height * aspectRatio
+	}
+	return { width: width, height: height }
+}
