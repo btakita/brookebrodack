1045ff354269ee811e74d89bd0c68fe51769de46
Brian Takita <brian.takita@gmail.com>
2024-03-13 01:10:58 -0400

    -brookebrodack-dev-bin: tmux-dev:
    	- tmux rename-window dev
    	top-right: bun -b run dev: cd outside of send-keys

--- app/-brookebrodack-dev-bin/bin/tmux-dev.sh
+++ app/-brookebrodack-dev-bin/bin/tmux-dev.sh
@@ -3,13 +3,13 @@ DIR_DEFAULT=$(dirname $0)/../..
 DIR="${DIR:-$DIR_DEFAULT}"
 
 cd $DIR
-tmux rename-window dev
 tmux split-window -v $SHELL
 tmux send-keys 'tig' 'C-m'
 tmux split-window -h $SHELL
 tmux select-pane -t 1
 tmux split-window -h $SHELL
-tmux send-keys "cd $DIR/app/brookebrodack-site; bun -b run dev" 'C-m'
+cd $DIR/app/brookebrodack-site
+tmux send-keys "bun -b run dev" 'C-m'
 tmux select-pane -t 1
 
 tmux rename-window brookebrodack
